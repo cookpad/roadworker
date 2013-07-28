ee0e58a54469a1f67acf222129056f3b65c3b5a8
winebarrel <sgwr_dts@yahoo.co.jp>
2013-07-29 02:17:12 +0900

    update spec

--- spec/roadworker_spec.rb
+++ spec/roadworker_spec.rb
@@ -28,32 +28,33 @@ describe Roadworker::Client do
     }
   end
 
-  context 'Create HostedZone' do
-    before do
-      routefile do
+  context 'Create' do
+    context 'HostedZone' do
+      before do
+        routefile do
 <<EOS
 hosted_zone "winebarre.jp" do
 end
 EOS
+        end
       end
+
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
+
+        zone = zones[0]
+        expect(zone.name).to eq("winebarre.jp.")
+        expect(zone.resource_record_set_count).to eq(2)
+
+        expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
+      }
     end
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
-
-      zone = zones[0]
-      expect(zone.name).to eq("winebarre.jp.")
-      expect(zone.resource_record_set_count).to eq(2)
-
-      expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
-    }
-  end
-
-  context 'Create A record' do
-    before {
-      routefile do
+    context 'A record' do
+      before {
+        routefile do
 <<EOS
 hosted_zone "winebarre.jp" do
   rrset "www.winebarre.jp", "A" do
@@ -65,30 +66,30 @@ hosted_zone "winebarre.jp" do
   end
 end
 EOS
-      end
-    }
+        end
+      }
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
 
-      zone = zones[0]
-      expect(zone.name).to eq("winebarre.jp.")
-      expect(zone.resource_record_set_count).to eq(3)
+        zone = zones[0]
+        expect(zone.name).to eq("winebarre.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
 
-      expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
+        expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
 
-      a = zone.rrsets['www.winebarre.jp.', 'A']
-      expect(a.name).to eq("www.winebarre.jp.")
-      expect(a.ttl).to eq(123)
-      expect(rrs_list(a.resource_records)).to eq(["127.0.0.1", "127.0.0.2"])
-    }
-  end
+        a = zone.rrsets['www.winebarre.jp.', 'A']
+        expect(a.name).to eq("www.winebarre.jp.")
+        expect(a.ttl).to eq(123)
+        expect(rrs_list(a.resource_records)).to eq(["127.0.0.1", "127.0.0.2"])
+      }
+    end
 
-  context 'Create A(Alias) record' do
-    before {
-      routefile do
+    context 'A(Alias) record' do
+      before {
+        routefile do
 <<EOS
 hosted_zone "winebarre.jp" do
   rrset "www.winebarre.jp", "A" do
@@ -96,33 +97,33 @@ hosted_zone "winebarre.jp" do
   end
 end
 EOS
-      end
-    }
+        end
+      }
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
 
-      zone = zones[0]
-      expect(zone.name).to eq("winebarre.jp.")
-      expect(zone.resource_record_set_count).to eq(3)
+        zone = zones[0]
+        expect(zone.name).to eq("winebarre.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
 
-      expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
+        expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
 
-      a = zone.rrsets['www.winebarre.jp.', 'A']
-      expect(a.name).to eq("www.winebarre.jp.")
-      expect(a.alias_target).to eq({
-        :hosted_zone_id => "Z2YN17T5R711GT",
-        :dns_name => "roadworker-1957159880.ap-northeast-1.elb.amazonaws.com.",
-        :evaluate_target_health => false,
-      })
-    }
-  end
+        a = zone.rrsets['www.winebarre.jp.', 'A']
+        expect(a.name).to eq("www.winebarre.jp.")
+        expect(a.alias_target).to eq({
+          :hosted_zone_id => "Z2YN17T5R711GT",
+          :dns_name => "roadworker-1957159880.ap-northeast-1.elb.amazonaws.com.",
+          :evaluate_target_health => false,
+        })
+      }
+    end
 
-  context 'Create A1 A2' do
-    before {
-      routefile do
+    context 'A1 A2' do
+      before {
+        routefile do
 <<-EOS
 hosted_zone "winebarre.jp" do
   rrset "www.winebarre.jp", "A" do
@@ -146,39 +147,39 @@ hosted_zone "winebarre.jp" do
   end
 end
 EOS
-      end
-    }
+        end
+      }
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
 
-      zone = zones[0]
-      expect(zone.name).to eq("winebarre.jp.")
-      expect(zone.resource_record_set_count).to eq(4)
+        zone = zones[0]
+        expect(zone.name).to eq("winebarre.jp.")
+        expect(zone.resource_record_set_count).to eq(4)
 
-      expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
+        expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
 
-      a1 = zone.rrsets['www.winebarre.jp.', 'A', "web server 1"]
-      expect(a1.name).to eq("www.winebarre.jp.")
-      expect(a1.set_identifier).to eq('web server 1')
-      expect(a1.weight).to eq(100)
-      expect(a1.ttl).to eq(456)
-      expect(rrs_list(a1.resource_records)).to eq(["127.0.0.1", "127.0.0.2"])
+        a1 = zone.rrsets['www.winebarre.jp.', 'A', "web server 1"]
+        expect(a1.name).to eq("www.winebarre.jp.")
+        expect(a1.set_identifier).to eq('web server 1')
+        expect(a1.weight).to eq(100)
+        expect(a1.ttl).to eq(456)
+        expect(rrs_list(a1.resource_records)).to eq(["127.0.0.1", "127.0.0.2"])
 
-      a2 = zone.rrsets['www.winebarre.jp.', 'A', "web server 2"]
-      expect(a2.name).to eq("www.winebarre.jp.")
-      expect(a2.set_identifier).to eq('web server 2')
-      expect(a2.weight).to eq(50)
-      expect(a2.ttl).to eq(456)
-      expect(rrs_list(a2.resource_records)).to eq(["127.0.0.3", "127.0.0.4"])
-    }
-  end
+        a2 = zone.rrsets['www.winebarre.jp.', 'A', "web server 2"]
+        expect(a2.name).to eq("www.winebarre.jp.")
+        expect(a2.set_identifier).to eq('web server 2')
+        expect(a2.weight).to eq(50)
+        expect(a2.ttl).to eq(456)
+        expect(rrs_list(a2.resource_records)).to eq(["127.0.0.3", "127.0.0.4"])
+      }
+    end
 
-  context 'Create TXT record' do
-    before {
-      routefile do
+    context 'TXT record' do
+      before {
+        routefile do
 <<EOS
 hosted_zone "winebarre.jp" do
   rrset "www.winebarre.jp", "TXT" do
@@ -191,34 +192,34 @@ hosted_zone "winebarre.jp" do
   end
 end
 EOS
-      end
-    }
+        end
+      }
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
 
-      zone = zones[0]
-      expect(zone.name).to eq("winebarre.jp.")
-      expect(zone.resource_record_set_count).to eq(3)
+        zone = zones[0]
+        expect(zone.name).to eq("winebarre.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
 
-      expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
+        expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
 
-      txt = zone.rrsets['www.winebarre.jp.', 'TXT']
-      expect(txt.name).to eq("www.winebarre.jp.")
-      expect(txt.ttl).to eq(123)
-      expect(rrs_list(txt.resource_records)).to eq([
-        "\"spf2.0/mfrom +ip4:192.168.100.0/24 ~all\"",
-        "\"spf2.0/pra +ip4:192.168.100.0/24 ~all\"",
-        "\"v=spf1 +ip4:192.168.100.0/24 ~all\""
-      ])
-    }
-  end
+        txt = zone.rrsets['www.winebarre.jp.', 'TXT']
+        expect(txt.name).to eq("www.winebarre.jp.")
+        expect(txt.ttl).to eq(123)
+        expect(rrs_list(txt.resource_records)).to eq([
+          "\"spf2.0/mfrom +ip4:192.168.100.0/24 ~all\"",
+          "\"spf2.0/pra +ip4:192.168.100.0/24 ~all\"",
+          "\"v=spf1 +ip4:192.168.100.0/24 ~all\""
+        ])
+      }
+    end
 
-  context 'Create CNAME record' do
-    before {
-      routefile do
+    context 'CNAME record' do
+      before {
+        routefile do
 <<EOS
 hosted_zone "winebarre.jp" do
   rrset "www.winebarre.jp", "CNAME" do
@@ -227,30 +228,30 @@ hosted_zone "winebarre.jp" do
   end
 end
 EOS
-      end
-    }
+        end
+      }
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
 
-      zone = zones[0]
-      expect(zone.name).to eq("winebarre.jp.")
-      expect(zone.resource_record_set_count).to eq(3)
+        zone = zones[0]
+        expect(zone.name).to eq("winebarre.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
 
-      expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
+        expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
 
-      cname = zone.rrsets['www.winebarre.jp.', 'CNAME']
-      expect(cname.name).to eq("www.winebarre.jp.")
-      expect(cname.ttl).to eq(123)
-      expect(rrs_list(cname.resource_records)).to eq(["www2.winebarre.jp"])
-    }
-  end
+        cname = zone.rrsets['www.winebarre.jp.', 'CNAME']
+        expect(cname.name).to eq("www.winebarre.jp.")
+        expect(cname.ttl).to eq(123)
+        expect(rrs_list(cname.resource_records)).to eq(["www2.winebarre.jp"])
+      }
+    end
 
-  context 'Create MX record' do
-    before {
-      routefile do
+    context 'MX record' do
+      before {
+        routefile do
 <<EOS
 hosted_zone "winebarre.jp" do
   rrset "www.winebarre.jp", "MX" do
@@ -262,30 +263,30 @@ hosted_zone "winebarre.jp" do
   end
 end
 EOS
-      end
-    }
+        end
+      }
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
 
-      zone = zones[0]
-      expect(zone.name).to eq("winebarre.jp.")
-      expect(zone.resource_record_set_count).to eq(3)
+        zone = zones[0]
+        expect(zone.name).to eq("winebarre.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
 
-      expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
+        expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
 
-      mx = zone.rrsets['www.winebarre.jp.', 'MX']
-      expect(mx.name).to eq("www.winebarre.jp.")
-      expect(mx.ttl).to eq(123)
-      expect(rrs_list(mx.resource_records)).to eq(["10 mail.winebarre.jp", "20 mail2.winebarre.jp"])
-    }
-  end
+        mx = zone.rrsets['www.winebarre.jp.', 'MX']
+        expect(mx.name).to eq("www.winebarre.jp.")
+        expect(mx.ttl).to eq(123)
+        expect(rrs_list(mx.resource_records)).to eq(["10 mail.winebarre.jp", "20 mail2.winebarre.jp"])
+      }
+    end
 
-  context 'Create PTR record' do
-    before {
-      routefile do
+    context 'PTR record' do
+      before {
+        routefile do
 <<EOS
 hosted_zone "333.222.111.in-addr.arpa" do
   rrset "444.333.222.111.in-addr.arpa", "PTR" do
@@ -294,30 +295,30 @@ hosted_zone "333.222.111.in-addr.arpa" do
   end
 end
 EOS
-      end
-    }
+        end
+      }
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
 
-      zone = zones[0]
-      expect(zone.name).to eq("333.222.111.in-addr.arpa.")
-      expect(zone.resource_record_set_count).to eq(3)
+        zone = zones[0]
+        expect(zone.name).to eq("333.222.111.in-addr.arpa.")
+        expect(zone.resource_record_set_count).to eq(3)
 
-      expect(zone.rrsets['333.222.111.in-addr.arpa.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['333.222.111.in-addr.arpa.', 'SOA'].ttl).to eq(900)
+        expect(zone.rrsets['333.222.111.in-addr.arpa.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['333.222.111.in-addr.arpa.', 'SOA'].ttl).to eq(900)
 
-      ptr = zone.rrsets['444.333.222.111.in-addr.arpa.', 'PTR']
-      expect(ptr.name).to eq("444.333.222.111.in-addr.arpa.")
-      expect(ptr.ttl).to eq(123)
-      expect(rrs_list(ptr.resource_records)).to eq(["www.winebarre.jp"])
-    }
-  end
+        ptr = zone.rrsets['444.333.222.111.in-addr.arpa.', 'PTR']
+        expect(ptr.name).to eq("444.333.222.111.in-addr.arpa.")
+        expect(ptr.ttl).to eq(123)
+        expect(rrs_list(ptr.resource_records)).to eq(["www.winebarre.jp"])
+      }
+    end
 
-  context 'Create SRV record' do
-    before {
-      routefile do
+    context 'SRV record' do
+      before {
+        routefile do
 <<EOS
 hosted_zone "winebarre.jp" do
   rrset "ftp.winebarre.jp", "SRV" do
@@ -329,33 +330,33 @@ hosted_zone "winebarre.jp" do
   end
 end
 EOS
-      end
-    }
+        end
+      }
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
 
-      zone = zones[0]
-      expect(zone.name).to eq("winebarre.jp.")
-      expect(zone.resource_record_set_count).to eq(3)
+        zone = zones[0]
+        expect(zone.name).to eq("winebarre.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
 
-      expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
+        expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
 
-      srv = zone.rrsets['ftp.winebarre.jp.', 'SRV']
-      expect(srv.name).to eq("ftp.winebarre.jp.")
-      expect(srv.ttl).to eq(123)
-      expect(rrs_list(srv.resource_records)).to eq([
-        "1   0   21  server01.example.jp",
-        "2   0   21  server02.example.jp"
-      ])
-    }
-  end
+        srv = zone.rrsets['ftp.winebarre.jp.', 'SRV']
+        expect(srv.name).to eq("ftp.winebarre.jp.")
+        expect(srv.ttl).to eq(123)
+        expect(rrs_list(srv.resource_records)).to eq([
+          "1   0   21  server01.example.jp",
+          "2   0   21  server02.example.jp"
+        ])
+      }
+    end
 
-  context 'Create AAAA record' do
-    before {
-      routefile do
+    context 'AAAA record' do
+      before {
+        routefile do
 <<EOS
 hosted_zone "winebarre.jp" do
   rrset "www.winebarre.jp", "AAAA" do
@@ -364,24 +365,25 @@ hosted_zone "winebarre.jp" do
   end
 end
 EOS
-      end
-    }
+        end
+      }
 
-    it {
-      zones = @route53.hosted_zones.to_a
-      expect(zones.length).to eq(1)
+      it {
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
 
-      zone = zones[0]
-      expect(zone.name).to eq("winebarre.jp.")
-      expect(zone.resource_record_set_count).to eq(3)
+        zone = zones[0]
+        expect(zone.name).to eq("winebarre.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
 
-      expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
-      expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
+        expect(zone.rrsets['winebarre.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarre.jp.', 'SOA'].ttl).to eq(900)
 
-      aaaa = zone.rrsets['www.winebarre.jp.', 'AAAA']
-      expect(aaaa.name).to eq("www.winebarre.jp.")
-      expect(aaaa.ttl).to eq(123)
-      expect(rrs_list(aaaa.resource_records)).to eq(["::1"])
-    }
+        aaaa = zone.rrsets['www.winebarre.jp.', 'AAAA']
+        expect(aaaa.name).to eq("www.winebarre.jp.")
+        expect(aaaa.ttl).to eq(123)
+        expect(rrs_list(aaaa.resource_records)).to eq(["::1"])
+      }
+    end
   end
 end
