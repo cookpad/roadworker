0ee9288da814a5c9a8d07ef80ebdcf870d1c049b
winebarrel <sgwr_dts@yahoo.co.jp>
2013-07-29 21:04:23 +0900

    add return updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,17 @@
 def routefile(options = {})
+  updated = false
   tempfile = `mktemp /tmp/#{File.basename(__FILE__)}.XXXXXX`.strip
 
   begin
     open(tempfile, 'wb') {|f| f.puts(yield) }
     options = {:logger => Logger.new('/dev/null')}.merge(options)
     client = Roadworker::Client.new(options)
-    client.apply(tempfile)
+    updated = client.apply(tempfile)
   ensure
     FileUtils.rm_f(tempfile)
   end
+
+  return updated
 end
 
 def rrs_list(rrs)
