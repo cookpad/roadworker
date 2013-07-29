c0770c48b17adc697bb1774d23a22d100911fb21
winebarrel <sgwr_dts@yahoo.co.jp>
2013-07-29 23:52:39 +0900

    small fix

--- lib/roadworker/log.rb
+++ lib/roadworker/log.rb
@@ -3,7 +3,7 @@ module Roadworker
 
     def log(level, message, color, log_id = nil)
       log_id = yield if block_given?
-      message = "#{message}: #{log_id}"
+      message = "#{message}: #{log_id}" if log_id
       message << ' (dry-run)' if @options.dry_run
       @options.logger.send(level, message.send(color))
     end
