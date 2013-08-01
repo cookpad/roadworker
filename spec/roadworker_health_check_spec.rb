2d59b7e69280dc08404f9d15820a488c20860f9e
winebarrel <sgwr_dts@yahoo.co.jp>
2013-08-02 01:55:48 +0900

    update health check spec

--- spec/roadworker_health_check_spec.rb
+++ spec/roadworker_health_check_spec.rb
@@ -158,6 +158,65 @@ EOS
           })
         }
       end
+
+      context 'No Check' do
+        it {
+          routefile do
+<<EOS
+hosted_zone "winebarrel.jp" do
+  rrset "www.winebarrel.jp", "A" do
+    set_identifier "Primary"
+    failover "PRIMARY"
+    ttl 456
+    resource_records(
+      "127.0.0.1",
+      "127.0.0.2"
+    )
+  end
+
+  rrset "www.winebarrel.jp", "A" do
+    set_identifier "Secondary"
+    failover "SECONDARY"
+    ttl 456
+    resource_records(
+      "127.0.0.3",
+      "127.0.0.4"
+    )
+  end
+end
+EOS
+          end
+
+          zones = @route53.hosted_zones.to_a
+          expect(zones.length).to eq(1)
+
+          zone = zones[0]
+          expect(zone.name).to eq("winebarrel.jp.")
+          expect(zone.resource_record_set_count).to eq(4)
+
+          expect(zone.rrsets['winebarrel.jp.', 'NS'].ttl).to eq(172800)
+          expect(zone.rrsets['winebarrel.jp.', 'SOA'].ttl).to eq(900)
+
+          check_list = fetch_health_checks(@route53)
+          expect(check_list.length).to eq(1)
+
+          a1 = zone.rrsets['www.winebarrel.jp.', 'A', "Primary"]
+          expect(a1.name).to eq("www.winebarrel.jp.")
+          expect(a1.set_identifier).to eq('Primary')
+          expect(a1.failover).to eq('PRIMARY')
+          expect(a1.ttl).to eq(456)
+          expect(rrs_list(a1.resource_records)).to eq(["127.0.0.1", "127.0.0.2"])
+          expect(a1.health_check_id).to be_nil
+
+          a2 = zone.rrsets['www.winebarrel.jp.', 'A', "Secondary"]
+          expect(a2.name).to eq("www.winebarrel.jp.")
+          expect(a2.set_identifier).to eq('Secondary')
+          expect(a2.failover).to eq('SECONDARY')
+          expect(a2.ttl).to eq(456)
+          expect(rrs_list(a2.resource_records)).to eq(["127.0.0.3", "127.0.0.4"])
+          expect(b1.health_check_id).to be_nil
+        }
+      end
     end
   end
 end
