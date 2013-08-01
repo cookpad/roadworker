de333857d253d9bab04998fd44803a732ec0ec36
winebarrel <sgwr_dts@yahoo.co.jp>
2013-08-02 01:40:26 +0900

    add health check spec

---
+++ spec/roadworker_health_check_spec.rb
@@ -0,0 +1,79 @@
+$: << File.expand_path("#{File.dirname __FILE__}/../lib")
+$: << File.expand_path("#{File.dirname __FILE__}/../spec")
+
+require 'rubygems'
+require 'roadworker'
+require 'spec_helper'
+require 'fileutils'
+require 'logger'
+
+describe Roadworker::Client do
+  before(:each) {
+    routefile(:force => true) { '' }
+    @route53 = AWS::Route53.new
+  }
+
+  after(:all) do
+    routefile(:force => true) { '' }
+  end
+
+  describe 'HealthCheck' do
+    context 'Create' do
+      context 'HTTP' do
+        it {
+          routefile do
+<<EOS
+hosted_zone "winebarrel.jp" do
+  rrset "www.winebarrel.jp", "A" do
+    set_identifier "Primary"
+    failover "PRIMARY"
+    health_check "http://192.0.43.10:80/path"
+    ttl 456
+    resource_records(
+      "127.0.0.1",
+      "127.0.0.2"
+    )
+  end
+
+  rrset "www.winebarrel.jp", "A" do
+    set_identifier "Secondary"
+    failover "SECONDARY"
+    health_check "http://192.0.43.10:80/path"
+    ttl 456
+    resource_records(
+      "127.0.0.3",
+      "127.0.0.4"
+    )
+  end
+end
+EOS
+          end
+
+          zones = @route53.hosted_zones.to_a
+          expect(zones.length).to eq(1)
+
+          zone = zones[0]
+          expect(zone.name).to eq("winebarrel.jp.")
+          expect(zone.resource_record_set_count).to eq(4)
+
+          expect(zone.rrsets['winebarrel.jp.', 'NS'].ttl).to eq(172800)
+          expect(zone.rrsets['winebarrel.jp.', 'SOA'].ttl).to eq(900)
+
+          a1 = zone.rrsets['www.winebarrel.jp.', 'A', "Primary"]
+          expect(a1.name).to eq("www.winebarrel.jp.")
+          expect(a1.set_identifier).to eq('Primary')
+          expect(a1.failover).to eq('PRIMARY')
+          expect(a1.ttl).to eq(456)
+          expect(rrs_list(a1.resource_records)).to eq(["127.0.0.1", "127.0.0.2"])
+
+          a2 = zone.rrsets['www.winebarrel.jp.', 'A', "Secondary"]
+          expect(a2.name).to eq("www.winebarrel.jp.")
+          expect(a2.set_identifier).to eq('Secondary')
+          expect(a2.failover).to eq('SECONDARY')
+          expect(a2.ttl).to eq(456)
+          expect(rrs_list(a2.resource_records)).to eq(["127.0.0.3", "127.0.0.4"])
+        }
+      end
+    end
+  end
+end
