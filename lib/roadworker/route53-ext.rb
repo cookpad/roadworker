b796c4a3a233080b37b4691182ea5d7539b0d003
winebarrel <sgwr_dts@yahoo.co.jp>
2013-08-28 00:17:38 +0900

    support s3 alias target

--- lib/roadworker/route53-ext.rb
+++ lib/roadworker/route53-ext.rb
@@ -3,11 +3,27 @@ require 'aws-sdk'
 module AWS
   class Route53
 
+    # http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
+    S3_WEBSITE_ENDPOINTS = {
+      's3-website-us-east-1.amazonaws.com'      => 'Z3AQBSTGFYJSTF',
+      's3-website-us-west-2.amazonaws.com'      => 'Z3BJ6K6RIION7M',
+      's3-website-us-west-1.amazonaws.com'      => 'Z2F56UZL2M1ACD',
+      's3-website-eu-west-1.amazonaws.com'      => 'Z1BKCTXD74EZPE',
+      's3-website-ap-southeast-1.amazonaws.com' => 'Z3O0J2DXBE1FTB',
+      's3-website-ap-southeast-2.amazonaws.com' => 'Z1WCIGYICN2BYD',
+      's3-website-ap-northeast-1.amazonaws.com' => 'Z2M4EHUR26P7ZW',
+      's3-website-sa-east-1.amazonaws.com'      => 'Z7KQH4QJS55SO',
+      's3-website-us-gov-west-1.amazonaws.com'  => 'Z31GFT0UA1I2HV',
+    }
+
     class << self
       def dns_name_to_alias_target(name)
         name = name.sub(/\.\Z/, '')
 
-        if name =~ /([^.]+)\.elb\.amazonaws.com\Z/i
+        if (s3_hosted_zone_id = S3_WEBSITE_ENDPOINTS[name.downcase]) and name =~ /\As3-website-([^.]+).amazonaws.com\Z/i
+          region = $1.downcase
+          s3_dns_name_to_alias_target(name, region, hosted_zone_id)
+        elsif name =~ /([^.]+)\.elb\.amazonaws.com\Z/i
           region = $1.downcase
           elb_dns_name_to_alias_target(name, region)
         else
@@ -16,6 +32,14 @@ module AWS
       end
 
       private
+
+      def s3_dns_name_to_alias_target(name, region, rehosted_zone_id)
+        {
+          :hosted_zone_id         => hosted_zone_id,
+          :dns_name               => name,
+          :evaluate_target_health => false, # XXX:
+        }
+      end
 
       def elb_dns_name_to_alias_target(name, region)
         elb = AWS::ELB.new(:region => region)
