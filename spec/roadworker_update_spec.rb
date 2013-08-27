fa8a87d15d0bd4865842c6286051f3514477e679
winebarrel <sgwr_dts@yahoo.co.jp>
2013-08-28 00:30:24 +0900

    add s3 alias testcase

--- spec/roadworker_update_spec.rb
+++ spec/roadworker_update_spec.rb
@@ -258,6 +258,54 @@ EOS
       }
     end
 
+    context 'A record -> A(Alias) record (S3)' do
+      before {
+        routefile do
+<<EOS
+hosted_zone "winebarrel.jp" do
+  rrset "www.winebarrel.jp", "A" do
+    ttl 123
+    resource_records(
+      "127.0.0.1",
+      "127.0.0.2"
+    )
+  end
+end
+EOS
+        end
+      }
+
+      it {
+        routefile do
+<<EOS
+hosted_zone "winebarrel.jp" do
+  rrset "www.winebarrel.jp", "A" do
+    dns_name "s3-website-us-east-1.amazonaws.com"
+  end
+end
+EOS
+        end
+
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
+
+        zone = zones[0]
+        expect(zone.name).to eq("winebarrel.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
+
+        expect(zone.rrsets['winebarrel.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarrel.jp.', 'SOA'].ttl).to eq(900)
+
+        a = zone.rrsets['www.winebarrel.jp.', 'A']
+        expect(a.name).to eq("www.winebarrel.jp.")
+        expect(a.alias_target).to eq({
+          :hosted_zone_id => "Z3AQBSTGFYJSTF",
+          :dns_name => "s3-website-us-east-1.amazonaws.com.",
+          :evaluate_target_health => false,
+        })
+      }
+    end
+
     context 'A1 A2' do
       before {
         routefile do
