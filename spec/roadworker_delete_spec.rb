17cffeabcbaffede4184b3233ee346d5fa6c54dc
winebarrel <sgwr_dts@yahoo.co.jp>
2013-10-14 11:59:37 +0900

    add spf record test

--- spec/roadworker_delete_spec.rb
+++ spec/roadworker_delete_spec.rb
@@ -1046,6 +1046,64 @@ EOS
       }
     end
 
+    context 'SPF record' do
+      before {
+        routefile do
+<<EOS
+hosted_zone "winebarrel.jp" do
+  rrset "info.winebarrel.jp", "A" do
+    ttl 123
+    resource_records(
+      "127.0.0.1",
+      "127.0.0.2"
+    )
+  end
+
+  rrset "www.winebarrel.jp", "SPF" do
+    ttl 123
+    resource_records(
+      '"v=spf1 +ip4:192.168.100.0/24 ~all"',
+      '"spf2.0/pra +ip4:192.168.100.0/24 ~all"',
+      '"spf2.0/mfrom +ip4:192.168.100.0/24 ~all"'
+    )
+  end
+end
+EOS
+        end
+      }
+
+      it {
+        routefile do
+<<EOS
+hosted_zone "winebarrel.jp" do
+  rrset "info.winebarrel.jp", "A" do
+    ttl 123
+    resource_records(
+      "127.0.0.1",
+      "127.0.0.2"
+    )
+  end
+end
+EOS
+        end
+
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
+
+        zone = zones[0]
+        expect(zone.name).to eq("winebarrel.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
+
+        expect(zone.rrsets['winebarrel.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarrel.jp.', 'SOA'].ttl).to eq(900)
+
+        info = zone.rrsets['info.winebarrel.jp.', 'A']
+        expect(info.name).to eq("info.winebarrel.jp.")
+        expect(info.ttl).to eq(123)
+        expect(rrs_list(info.resource_records)).to eq(["127.0.0.1", "127.0.0.2"])
+      }
+    end
+
     context 'NS record' do
       before {
         routefile do
