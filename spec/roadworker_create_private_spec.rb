8eab407da06d16e4a02d2a2abc472f5a6eda0dd8
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-11-08 17:47:33 +0900

    Add private hz spec

---
+++ spec/roadworker_create_private_spec.rb
@@ -0,0 +1,44 @@
+describe Roadworker::Client do
+  context 'Create private' do
+    context 'A record' do
+      it {
+        routefile do
+<<EOS
+hosted_zone "winebarrel.jp" do
+  vpc TEST_VPC_REGION, TEST_VPC1
+  vpc TEST_VPC_REGION, TEST_VPC2
+
+  rrset "www.winebarrel.jp", "A" do
+    ttl 123
+    resource_records(
+      "127.0.0.1",
+      "127.0.0.2"
+    )
+  end
+end
+EOS
+        end
+
+        zones = @route53.hosted_zones.to_a
+        expect(zones.length).to eq(1)
+
+        zone = zones[0]
+        expect(zone.name).to eq("winebarrel.jp.")
+        expect(zone.resource_record_set_count).to eq(3)
+
+        expect(zone.rrsets['winebarrel.jp.', 'NS'].ttl).to eq(172800)
+        expect(zone.rrsets['winebarrel.jp.', 'SOA'].ttl).to eq(900)
+
+        expect(zone.vpcs).to match_array [
+          {:vpc_region => TEST_VPC_REGION, :vpc_id => TEST_VPC1},
+          {:vpc_region => TEST_VPC_REGION, :vpc_id => TEST_VPC2},
+        ]
+
+        a = zone.rrsets['www.winebarrel.jp.', 'A']
+        expect(a.name).to eq("www.winebarrel.jp.")
+        expect(a.ttl).to eq(123)
+        expect(rrs_list(a.resource_records.sort_by {|i| i.to_s })).to eq(["127.0.0.1", "127.0.0.2"])
+      }
+    end
+  end
+end
