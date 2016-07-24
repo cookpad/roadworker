f6c39d1cff2fb5c96a31c7e9a46abcb37ee2ec7a
Genki Sugawara <sugawara@cookpad.com>
2016-07-24 17:43:14 +0900

    Fix s3 hosted zone id

--- lib/roadworker/route53-ext.rb
+++ lib/roadworker/route53-ext.rb
@@ -5,20 +5,22 @@ module Aws
 
     # http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
     S3_WEBSITE_ENDPOINTS = {
-      's3-website-us-east-1.amazonaws.com'      => 'z3AQBSTGFYJSTF',
-      's3-website-us-west-2.amazonaws.com'      => 'z3BJ6K6RIION7M',
-      's3-website-us-west-1.amazonaws.com'      => 'z2F56UZL2M1ACD',
-      's3-website-eu-west-1.amazonaws.com'      => 'z1BKCTXD74EZPE',
-      's3-website.eu-central-1.amazonaws.com'   => 'z21DNDUVLTQW6Q',
-      's3-website-ap-southeast-1.amazonaws.com' => 'z3O0J2DXBE1FTB',
-      's3-website-ap-southeast-2.amazonaws.com' => 'z1WCIGYICN2BYD',
-      's3-website-ap-northeast-1.amazonaws.com' => 'z2M4EHUR26P7ZW',
-      's3-website-sa-east-1.amazonaws.com'      => 'z7KQH4QJS55SO',
-      's3-website-us-gov-west-1.amazonaws.com'  => 'z31GFT0UA1I2HV',
+      's3-website-us-east-1.amazonaws.com' => 'Z3AQBSTGFYJSTF',
+      's3-website-us-west-1.amazonaws.com' => 'Z2F56UZL2M1ACD',
+      's3-website-us-west-2.amazonaws.com' => 'Z3BJ6K6RIION7M',
+      's3-website.ap-south-1.amazonaws.com' => 'Z11RGJOFQNVJUP',
+      's3-website.ap-northeast-2.amazonaws.com' => 'Z3W03O7B5YMIYP',
+      's3-website-ap-southeast-1.amazonaws.com' => 'Z3O0J2DXBE1FTB',
+      's3-website-ap-southeast-2.amazonaws.com' => 'Z1WCIGYICN2BYD',
+      's3-website-ap-northeast-1.amazonaws.com' => 'Z2M4EHUR26P7ZW',
+      's3-website.eu-central-1.amazonaws.com' => 'Z21DNDUVLTQW6Q',
+      's3-website-eu-west-1.amazonaws.com' => 'Z1BKCTXD74EZPE',
+      's3-website-sa-east-1.amazonaws.com' => 'Z7KQH4QJS55SO',
+      's3-website-us-gov-west-1.amazonaws.com' => 'Z31GFT0UA1I2HV',
     }
 
     # http://docs.aws.amazon.com/Route53/latest/APIReference/API_ChangeResourceRecordSets.html
-    CF_HOSTED_zONE_ID = 'Z2FDTNDATAQYW2'
+    CF_HOSTED_ZONE_ID = 'Z2FDTNDATAQYW2'
 
     class << self
       def normalize_dns_name_options(src)
@@ -100,7 +102,7 @@ module Aws
 
       def cf_dns_name_to_alias_target(name)
         {
-          :hosted_zone_id         => CF_HOSTED_zONE_ID,
+          :hosted_zone_id         => CF_HOSTED_ZONE_ID,
           :dns_name               => name,
           :evaluate_target_health => false, # XXX:
         }
